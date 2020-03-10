f43fda5774f92712c52863127ea94544c8096e3b
David Bond <david.bond@panoramicdata.com>
2020-03-10 00:33:01 +0000

    Added IntroducedInVersion attribute

---
+++ Rundeck.Api/IntroducedInVersionAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Rundeck.Api.Interfaces
+{
+	/// <summary>
+	/// The version of the API that the endpoint was introudcued in.
+	/// If set, the API will throw a NotSupportedException if the
+	/// version of Rundeck being queried is lower than this number.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+	internal class IntroducedInVersionAttribute : Attribute
+	{
+		public int InitialVersion { get; }
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="version"></param>
+		public IntroducedInVersionAttribute(int version)
+		{
+			InitialVersion = version;
+		}
+	}
+}
