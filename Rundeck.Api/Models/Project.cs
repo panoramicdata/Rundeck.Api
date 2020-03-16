aa8a30d15ad2b6e3c6f2e9978efb54d36b999ab7
David Bond <david.bond@panoramicdata.com>
2020-03-16 16:54:21 +0000

    Improved project model documentation.

--- Rundeck.Api/Models/Project.cs
+++ Rundeck.Api/Models/Project.cs
@@ -4,30 +4,32 @@ namespace Rundeck.Api.Models
 {
 	/// <summary>
 	/// A Project
+	/// Documentation: https://docs.rundeck.com/docs/administration/projects/
 	/// </summary>
 	[DataContract]
 	public class Project
 	{
 		/// <summary>
-		/// Description
+		/// The project description
 		/// </summary>
 		[DataMember(Name = "description")]
 		public string Description { get; set; } = default!;
 
 		/// <summary>
-		/// Name
+		/// The project name
 		/// </summary>
 		[DataMember(Name = "name")]
 		public string Name { get; set; } = default!;
 
 		/// <summary>
-		/// Url
+		/// The project URL
 		/// </summary>
 		[DataMember(Name = "url")]
 		public string Url { get; set; } = default!;
 
 		/// <summary>
-		/// Config
+		/// Project configuration.  For more details, see:
+		/// https://docs.rundeck.com/docs/administration/projects/configuration.html
 		/// </summary>
 		[DataMember(Name = "config")]
 		public Config Config { get; set; } = new Config();
