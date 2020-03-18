27324dace7ecbdcf48d6337a9f82ccbf71869fb0
Peter Acs <peter.acs@panoramic.systems>
2020-03-18 09:54:08 +0000

    Added test to list all installed Plugins

---
+++ Rundeck.Api.Test/Documented/PluginsTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Rundeck.Api.Test.Documented
+{
+	public class PluginsTests : TestBase
+	{
+		public PluginsTests(ITestOutputHelper output) : base(output)
+		{
+		}
+
+		[Fact]
+		public async void Plugins_GetAll_Passes()
+		{
+			var plugins = await RundeckClient
+				.Plugins
+				.GetAllAsync()
+				.ConfigureAwait(false);
+
+			plugins.Should().NotBeNull();
+		}
+	}
+}
